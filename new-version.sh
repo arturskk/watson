86e1c5c233c0b3a4f62d31e7d08efd0e586ae33e
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2017-11-26 00:32:51 +0100

    Release: 0.1.6

---
+++ new-version.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+RV=$1
+SV=$2
+
+if [ "x$RV" = "x" ] || [ "x$SV" = "x" ]; then
+    echo "Podaj wersje:"
+    echo "  new-version.sh wersja_release wersja_snapshot"
+    exit 1
+fi
+
+mvnw versions:set -DnewVersion="$RV"
+mvnw versions:commit
+
+mvnw clean install
+
+git add .   
+git commit -m "Release: $RV"
+git tag "v$RV"
+
+mvnw versions:set -DnewVersion="$SV"
+mvnw versions:commit
+
+git commit -m "Snapshot: $SV"
+
+git push
