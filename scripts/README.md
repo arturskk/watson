16cda2817449258285ab8c3a19b1220cb40ca4f0
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2017-12-04 22:36:08 +0100

    scripts

---
+++ scripts/README.md
@@ -0,0 +1,4 @@
+$ ls -la /etc/cron.hourly/watson-backupdb
+-rwxr-xr-x 1 root root 239 Nov 26 19:33 /etc/cron.hourly/watson-backupdb
+$ ls -la /etc/systemd/system/watson.service
+-rw-r--r-- 1 root root 177 Nov 26 19:41 /etc/systemd/system/watson.service
---
+++ scripts/watson-backupdb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pushd /opt/watson
+echo "Kopiowanie event logu watson"
+TARGET="events.log.`date '+%Y%m%d-%H%M%S'`"
+cp -a events.log "backup/$TARGET"
+
+echo "Event log skopiowany do /opt/watson/backup/$TARGET"
+ls -l "/opt/watson/backup/$TARGET"
