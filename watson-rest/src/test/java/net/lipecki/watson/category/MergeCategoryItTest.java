cc1091893cc515db496552a9c70fa28f4d9cc0b7
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2018-01-02 22:43:25 +0100

    Release: 0.2.7

---
+++ watson-rest/src/test/java/net/lipecki/watson/category/MergeCategoryItTest.java
@@ -0,0 +1,29 @@
+package net.lipecki.watson.category;
+
+import net.lipecki.watson.BaseItTest;
+import org.junit.Test;
+
+public class MergeCategoryItTest extends BaseItTest {
+
+    @Test
+    public void shouldMergeCategories() {
+        // given
+        // given two categories
+        // given product with category 2
+        // when
+        // category 2 merged int category 1
+        // then
+        // then product in category 1
+    }
+
+    @Test
+    public void shouldMoveProductsForMergedCategory() {
+        // given
+        // given category
+        // when
+        // category merged into root
+        // then
+        // then no category in category list
+    }
+
+}
