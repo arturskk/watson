e9680f426210bebe9a62aaf58a1aed5d07b4b31c
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2018-02-13 18:33:40 +0100

    Release: 0.4.8

--- build-version.sh
+++ build-version.sh
@@ -4,7 +4,7 @@ set -e
 
 ./mvnw versions:set -DremoveSnapshot
 ./mvnw versions:commit
-RELEASE_VERSION=`./mvnw help:evaluate -Dexpression=project.version 2>/dev/null | grep -v '\['`
+RELEASE_VERSION=`./mvnw help:evaluate -Dexpression=project.version 2>/dev/null | grep -v '\[' | sed -n 2p`
 
 ./mvnw clean package
 
@@ -14,7 +14,7 @@ git tag "v$RELEASE_VERSION"
 
 ./mvnw build-helper:parse-version versions:set -DnewVersion="${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion}-SNAPSHOT"
 ./mvnw versions:commit
-NEXT_SNAPSHOT_VERSION=`./mvnw help:evaluate -Dexpression=project.version 2>/dev/null | grep -v '\['`
+NEXT_SNAPSHOT_VERSION=`./mvnw help:evaluate -Dexpression=project.version 2>/dev/null | grep -v '\[' | sed -n 2p`
 
 git add .
 git commit -m "Preparing next snapshot: $NEXT_SNAPSHOT_VERSION"
