a7e51c1990c1883f6c0eba6e94f0e1e5cb202873
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2018-02-27 18:44:54 +0100

    skip optionals for jackson to json serialization

--- watson-business/src/main/java/net/lipecki/watson/WatsonBusinessConfiguration.java
+++ watson-business/src/main/java/net/lipecki/watson/WatsonBusinessConfiguration.java
@@ -1,5 +1,6 @@
 package net.lipecki.watson;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,6 +8,8 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import java.util.Optional;
 
 @Configuration
 @PropertySource("classpath:business-application.properties")
@@ -18,7 +21,12 @@ public class WatsonBusinessConfiguration {
         final ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.addMixIn(Optional.class, IgnoreOptionalJacksonMixIn.class);
         return objectMapper;
+    }
+
+    @JsonIgnoreType
+    public static class IgnoreOptionalJacksonMixIn {
     }
 
 }
